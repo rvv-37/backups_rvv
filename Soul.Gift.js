7d6233f24d8c5e58775a01decfb2fa49f6b1d34f
rvv <116062508+rvv-37@users.noreply.github.com>
2023-09-04 11:21:58 +0800

    Update and rename soul.cx.txt to Soul.Gift.js

---
+++ Soul.Gift.js
@@ -0,0 +1,3 @@
+[rewrite_local]
+^https:\/\/api-chat\.soulapp\.cn\/chat\/limitInfo url script-response-body https://raw.githubusercontent.com/Crazy-Z7/Script/main/Soul.js
+hostname = api-chat.soulapp.cn
