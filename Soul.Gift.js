31a53a852bc114973a64bef06808bb51ddcca7e0
rvv <116062508+rvv-37@users.noreply.github.com>
2024-04-12 23:51:58 +0800

    Delete Soul.Gift.js

--- Soul.Gift.js
+++
@@ -1,3 +0,0 @@
-[rewrite_local]
-^https:\/\/api-chat\.soulapp\.cn\/chat\/limitInfo url script-response-body https://raw.githubusercontent.com/Crazy-Z7/Script/main/Soul.js
-hostname = api-chat.soulapp.cn
